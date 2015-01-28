bdcb7d18b2008c31b2032a1bb70d90fd0b09995b
LE FRIOUX Ludovic <ludovic@lefrioux.fr>
2015-01-28 17:04:11 +0100

    partie 3 complète

--- src/application/Controler.java
+++ src/application/Controler.java
@@ -7,55 +7,56 @@ import peersim.core.*;
 import peersim.config.*;
 
 /*
-  Module d'initialisation de helloWorld: 
-  Fonctionnement:
-    pour chaque noeud, le module fait le lien entre la couche transport et la couche applicative
-    ensuite, il fait envoyer au noeud 0 un message "Hello" a tous les autres noeuds
- */
-public class Controler implements peersim.core.Control {
-    
-    private int applicationPid;
+   Module d'initialisation de helloWorld: 
+Fonctionnement:
+pour chaque noeud, le module fait le lien entre la couche transport et la couche applicative
+ensuite, il fait envoyer au noeud 0 un message "Hello" a tous les autres noeuds
+*/
+public class Controler implements peersim.core.Control
+{
+        private int applicationPid;
 
-    private boolean test=false;
+        private boolean test=false;
 
-    public Controler(String prefix) {
-	//recuperation du pid de la couche applicative
-	this.applicationPid = Configuration.getPid(prefix + ".applicationProtocolPid");
+        public Controler(String prefix) {
+                //recuperation du pid de la couche applicative
+                this.applicationPid = Configuration.getPid(prefix + ".applicationProtocolPid");
 
-    }
+        }
 
-    public boolean execute() {
-		int nodeNb, node, temp;		
-		Random rand = new Random();
-		Node dest;
-	
-		//recuperation de la taille du reseau
-		nodeNb = Network.size();
+        public boolean execute()
+        {
+                int nodeNb, node, temp;		
+                Random rand = new Random();
+                Node dest;
 
-		//System.out.println("coucou from Controler");
-		//creation du message
-/*		if(test==false){
-			if(rand.nextInt(100)<probaFaute){
-				int delay = 0;
-				if(rand.nextInt(100)<probaPas){
-					delay = 1;
-				}
-				test=true;
-				node = rand.nextInt(nodeNb);
-				System.out.println("kill de "+node);
-				Message message = new Message(Message.KILL,"A+ SOUS L'BUS",-1);
-				EDSimulator.add(delay, message, Network.get(node), helloWorldPid);
-				//Network.get(node).setFailState(Fallible.DEAD);
-			}else{
-				//System.out.println("pas de kill");
-			}
-		}
-*/		
-		if(CommonState.getTime() == 20){	
-			EDSimulator.add(500, Message(Message.DIE, "<A+ sous l'bus>", -1), applicationPid);
-		}
-		
-		
-		return false;
-    }
+                //recuperation de la taille du reseau
+                nodeNb = Network.size();
+
+                //System.out.println("coucou from Controler");
+                //creation du message
+                /*		if(test==false){
+                                if(rand.nextInt(100)<probaFaute){
+                                int delay = 0;
+                                if(rand.nextInt(100)<probaPas){
+                                delay = 1;
+                                }
+                                test=true;
+                                node = rand.nextInt(nodeNb);
+                                System.out.println("kill de "+node);
+                                Message message = new Message(Message.KILL,"A+ SOUS L'BUS",-1);
+                                EDSimulator.add(delay, message, Network.get(node), helloWorldPid);
+                //Network.get(node).setFailState(Fallible.DEAD);
+                }else{
+                //System.out.println("pas de kill");
+                }
+                }
+                */		
+                if(CommonState.getTime() == 20)
+                {	
+                        EDSimulator.add(500, new Message(Message.DIE, "<A+ sous l'bus>", -1), Network.get(2), this.applicationPid);
+                }
+
+                return false;
+        }
 }
